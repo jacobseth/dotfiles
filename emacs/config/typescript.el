14cbd1aa55b9b5931d11a44a7b87976d2d4294d5
Jacob McKenzie <jacob.mckenzie@opengear.com>
2020-03-18 08:06:21 +1000

    Customise typescript config

--- emacs/config/typescript.el
+++ emacs/config/typescript.el
@@ -9,14 +9,12 @@
   :ensure t
   :after (typescript-mode company flycheck)
   :hook ((typescript-mode . tide-setup)
-         (typescript-mode . tide-hl-identifier-mode)
-         (before-save . tide-format-before-save)))
+         (typescript-mode . tide-hl-identifier-mode)))
 
 (defun setup-tide-mode ()
   (interactive)
   (tide-setup)
   (flycheck-mode +1)
-  (setq flycheck-check-syntax-automatically '(save mode-enabled))
   (eldoc-mode +1)
   (tide-hl-identifier-mode +1)
   ;; company is an optional dependency. You have to
@@ -27,9 +25,6 @@
 ;; aligns annotation to the right hand side
 (setq company-tooltip-align-annotations t)
 
-;; formats the buffer before saving
-(add-hook 'before-save-hook 'tide-format-before-save)
-
 (add-hook 'typescript-mode-hook #'setup-tide-mode)
 
 (require 'web-mode)
@@ -37,8 +32,20 @@
 (add-hook 'web-mode-hook
           (lambda ()
             (when (string-equal "tsx" (file-name-extension buffer-file-name))
-              (setup-tide-mode))))
+              (setup-tide-mode)
+              (setq web-mode-css-indent-offset 2)
+              (setq web-mode-code-indent-offset 2)
+              (setq web-mode-indent-style 2)
+              (setq web-mode-markup-indent-offset 2))))
+
 ;; enable typescript-tslint checker
 (flycheck-add-mode 'typescript-tslint 'web-mode)
+
+
+(setq typescript-indent-level 2
+      typescript-expr-indent-offset 2)
+
+;; Stop auto-quoting after '=' in tsx
+(setq web-mode-enable-auto-quoting nil)
 
 (provide 'typescript)
